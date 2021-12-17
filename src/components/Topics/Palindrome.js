cab8e8459b9670c386b813c3f7beabca7f5d3cfe
Tomas Fisher <dontomassino777@gmail.com>
2021-12-17 13:08:00 -0700

    palindrome feature works

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,24 +1,43 @@
 import React, {Component} from 'react';
 
 export default class Palindrome extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            userInput: '',
+            palindrome: '',
+        }
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val })
+    }
+
+    isPalindrome(userInput) {
+        let testStr = userInput
+        let revStr = []
+
+        for (let i=0; i<testStr.length; i++) {
+            revStr.unshift(testStr[i])
+        }
+
+        if (revStr.join('') === testStr) {
+            this.setState({ palindrome: 'true'})
+        } else {
+            this.setState({ palindrome: 'false'})
+        }
+
+    }
+
     render() {
         return (
-            <div>
-                <h4>
-
-                </h4>
-                <input>
-
-                </input>
-                <button>
-
-                </button>
-                <span>
-
-                </span>
-                <span>
-                    
-                </span>
+            <div className='puzzleBox filterStringPB'>
+                <h4>Palindrome?</h4>
+                <input className='inputLine' onChange={ (e) => this.handleChange(e.target.value) }></input>
+                <button className='confirmationButton'onClick={ () => this.isPalindrome(this.state.userInput) }>Check</button>
+                <span className='resultsBox'>Palindrome: { JSON.stringify(this.state.palindrome)}</span>
             </div>
         )
     }
