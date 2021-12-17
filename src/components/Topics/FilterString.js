b50922c1dd0e71217f9e318372cd5e5566ce6777
Tomas Fisher <dontomassino777@gmail.com>
2021-12-17 12:34:39 -0700

    filter string working

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
 
 export default class FilterString extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            colors: ['red', 'indigo', 'violet', 'cyan'],
+            userInput: "",
+            filteredColors: []
+        }
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val});
+    }
+
+    filterColors(userInput) {
+        let colors = this.state.colors;
+        let filteredColors = [];
+
+        for (let i = 0; i < colors.length; i++) {
+            if (colors[i].includes(userInput)) {
+                filteredColors.push(colors[i]);
+            }
+        }
+
+        this.setState({ filteredColors: filteredColors})
+    }
+
     render() {
         return (
-            <div>
-                <h4>
-
-                </h4>
-                <input>
-
-                </input>
-                <button>
-
-                </button>
-                <span>
-
-                </span>
-                <span>
-                    
-                </span>
+            <div className="puzzleBox filterStringPB">
+                <h4>Filter String</h4>
+                <span className="puzzleText">Colors: { JSON.stringify(this.state.colors, null, 10) }</span>
+                <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+                <button className='confirmationButton' onClick={ () => this.filterColors(this.state.userInput) }>Filter</button>
+                <span className='resultsBox filterStringRB'>Filtered Colors: { JSON.stringify(this.state.filteredColors, null, 10) }</span>
             </div>
         )
     }
